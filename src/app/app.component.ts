27d98484a78959f9c29aa5bd69b882ab3351b5d1
orlinwilliams <orlinwilliams0077@gmail.com>
2021-02-17 02:34:44 +0000

    init tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'acklen-pair-session';
+
+  data:any = {number1:0, number2:0}
+  result: number = 0;
+
+  sum(){
+    this.result = this.data.number1 + this.data.number2;
+  }
 }
